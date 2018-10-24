fe3d96b0258fe7c1a156d62f8998db7b2fc97754
zpdelong <zacherypd@gmail.com>
2018-10-23 21:02:16 -0400

    Search function
    Implemented a basic search function similar to the reverse function from the
    other day.
    
    modified:   c2.lisp

--- c2.lisp
+++ c2.lisp
@@ -7,3 +7,16 @@
 (defvar l '(Jack Sprat could eat no chicken fat))
 
 (print (lat? l))
+
+(defun simple-search (element list)
+  (if (eql (car list) element)
+      t
+    (simple-search element (cdr list))))
+
+(defvar a '(1 2 3 4))
+
+(print '(Should be true))
+(print (simple-search 2 a))
+
+(print '(Should be false))
+(print (simple-search 5 a))
