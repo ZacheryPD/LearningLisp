f09fd2263303cc315a57a307d520d6b9bdb9d356
zpdelong <zacherypd@gmail.com>
2019-01-19 00:51:21 -0500

    Prototyping
    Adding examples that show...
    - Variables
    - Lambdas
    - The lambda shortcut

--- c2.scm
+++ c2.scm
@@ -1,10 +1,30 @@
 ;;;; Chapter 2 examples
+;;; Note: run this file with 'mit-scheme --quiet < c2.scm'
 
-;;; lat?
-(defun lat? (l)
-  (cond
-   ((null? l) #t
-    ((atom? (car l) (lat?(crd l)))
-     (else #f)))))
+;; You can define variables using the "define" function
+(define my-favorite-number 13)
 
-(print (lat? '(bacon and eggs)))
+;; You can define strings using double-quotes
+(define my-favorite-color "green")
+
+;; you define functions with lambdas.  Functions auto return their last line
+(define my-favorites
+  (lambda (color number)
+    (string-append "My favorite color is " color " and my favorite number is " number)))
+
+; display will print
+(display
+ (my-favorites my-favorite-color (number->string my-favorite-number)))
+
+; and newline prints a newline
+(newline)
+
+;; You can also use this shortcut to make lambdas
+(define (greet name)
+  (string-append "Hello, " name))
+
+(display (greet "World")) ; For some reason, though, this second display doesn't work (with or without the shortcut)
+
+
+
+
