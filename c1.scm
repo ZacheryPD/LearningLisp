777442118249cf61e24adf97fd8a1c5ac6274f09
zpdelong <zacherypd@gmail.com>
2019-10-30 20:00:12 -0400

    Adding comment explaining how to run code & basic explanation

--- c1.scm
+++ c1.scm
@@ -1,4 +1,9 @@
 ;;;; Chapter 1 notes
+;; To run this file, install mit-scheme and run scheme "--quiet < c1.scm"
+;; The display function can be used to print output to the screen
+(display "Use the \"display\" function to print an input to the screen")
+
+
 
 (display "There are only a few baisc expressions in Lisp")
 (car '(a b c))
