5252faec14afbf1a54ba031baa72afe7c37e7801
zpdelong <zacherypd@gmail.com>
2019-11-04 21:10:19 -0500

    Fixing bit about lists

--- c1.scm
+++ c1.scm
@@ -2,8 +2,15 @@
 ;; To run this file, install mit-scheme and run scheme "--quiet < c1.scm"
 ;; The display function can be used to print output to the screen
 (display "Use the \"display\" function to print an input to the screen")
+(newline)
+;; An atom is a string of characters
+(display "Atoms can start with anything that isn't \( or \)") (newline)
+(display "Atoms can be called using the \"quote\" method, or,") (newline)
+(display "apparently, a single quote") (newline)
+(quote atom)
+'atom
 
-
-
-(display "There are only a few baisc expressions in Lisp")
-(car '(a b c))
+;; A list is the other basic unit
+; A list is an atom, or multiple atoms enclosed by parentheses
+(quote (atom)) ; a single atom enclosed by parentheses
+'(another) ; another single atom enclosed by parentheses
