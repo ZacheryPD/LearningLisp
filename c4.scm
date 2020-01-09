9aa882200c5a8e6a38dc4d81a36fa218e150204f
zpdelong <zacherypd@gmail.com>
2020-01-08 23:10:19 -0500

    Adding C4 file

---
+++ c4.scm
@@ -0,0 +1,9 @@
+;;;; Chapter 4 examples
+;;; Note: run this file with 'mit-scheme --quiet < c4.scm'
+
+;; Helpers
+                                        ; Print something and a newline
+(define display-line
+  (lambda (input)
+    (display input)
+    (newline)))
