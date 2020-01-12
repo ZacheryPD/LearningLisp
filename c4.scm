d539312c9ba189e998b9c3912f2b3aa590d4b20c
zpdelong <zacherypd@gmail.com>
2020-01-12 16:17:33 -0500

    Adding basic arithmetic methods

--- c4.scm
+++ c4.scm
@@ -7,3 +7,37 @@
   (lambda (input)
     (display input)
     (newline)))
+
+;; Chapter-specific helpers
+
+; A method that adds one to the number given
+(define add1
+ (lambda (n)
+   (+ n 1)))
+(display "5 + 1: ")
+(display-line (add1 5))
+
+; A method that subtracts one from the number given
+(define sub1
+  (lambda (n)
+    (- n 1)))
+(display "5 - 1: ")
+(display-line (sub1 5))
+
+; A method that adds two integers
+(define add
+  (lambda (a b)
+    (cond
+     ((zero? b) a)
+     (else (add1 (add a (sub1 b)))))))
+(display "5 + 6: ")
+(display-line (+ 5 6))
+
+; A method that subtracts two integers
+(define sub
+  (lambda (a b)
+    (cond
+     ((zero? b) a)
+      (else (sub1 (sub a (sub1 b)))))))
+(display "5 - 4: ")
+(display-line (sub 5 4))
